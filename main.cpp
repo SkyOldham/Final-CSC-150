dff58a1812fe5093a3e7def2b33b36feffffad65
Aarron-Wilson <aawils03@wsc.edu>
2016-12-02 10:30:12 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -34,6 +34,7 @@ int main()
 void printIntro()
 {
 	cout << "Welcome to College Intramurals." << endl;
+	cout << "The Team Of Talanted Developers Are: Orion Hidy, Sky Oldon, Keaton Harpan" << endl;
 	cout << "At any time press -1 to exit and -2 to go back a step." << endl;
 }
 
